94267725d1ab984e929969119012aae7f89972e8
Sixtine Dromigny <sixtine.dromigny@gmail.com>
2023-10-23 12:04:37 +0100

    finished import of DWA, need to accelerate process of conversion to CSV/import in general, but at least data is accessible!

--- code/import_calculations.py
+++ code/import_calculations.py
@@ -1,8 +1,19 @@
-import obspy.taup
+import numpy as np
 from obspy.taup import TauPyModel
+import matplotlib as plt
 
 model = TauPyModel(model="prem")
 
 arrivals = model.get_travel_times(source_depth_in_km=55,
                                   distance_in_degree=67)
 
+
+import pandas as pd
+
+# Replace 'DWAK.bm' with the actual path to your .bm file
+file_path = 'DWAK.csv'
+
+# Read the .bm file into a DataFrame
+df = pd.read_csv(file_path,header=None)
+
+print(df)
